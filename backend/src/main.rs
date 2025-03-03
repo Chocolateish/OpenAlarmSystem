f1c871f74996b9b874a23d82016f0dd5d3c43248
Nicklas Nielsen <nicknielsen1995@hotmail.com>
2025-03-03 22:11:07 +0100

    a

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,49 +1,49 @@
 use std::net::TcpListener;
 use std::sync::{mpsc, Arc, RwLock};
-use std::thread::spawn;
+use std::thread::{sleep, spawn};
 use tungstenite::accept;
 
-struct Value {
-    value: i32,
-    b: i32,
-}
-
-struct ValueSet {
-    id: usize,
-    value: i32,
-}
-
-fn value_manager_setup() -> (mpsc::Sender<ValueSet>, Arc<RwLock<Vec<i32>>>) {
-    let values_i32: Arc<RwLock<Vec<i32>>> = Arc::new(RwLock::new(vec![]));
-    let (tx, rx): (mpsc::Sender<ValueSet>, mpsc::Receiver<ValueSet>) = mpsc::channel();
-    let values_clone = Arc::clone(&values_i32);
-    //Spawns value manager
-    spawn(move || loop {
-        let value = rx.recv().unwrap();
-        let mut values = values_clone.write().unwrap();
-        values[value.id] = value.value;
-    });
-    return (tx, values_i32);
+struct ValueMeta {
+    owner: i32,
 }
 
 /// A WebSocket echo server
 fn main() {
-    let valueChanger = value_manager_setup();
+    let mut testi32: Vec<RwLock<i32>> = Vec::new();
+    let mut testi32Meta: Vec<RwLock<ValueMeta>> = Vec::new();
 
-    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
-
-    for stream in server.incoming() {
-        spawn(move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-            loop {
-                let msg = websocket.read().unwrap();
-
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    print!("Received: {}", msg);
-                    websocket.send(msg).unwrap();
-                }
-            }
-        });
+    fn register_value(index: Option<usize>, _owner: i32) {
+        let index = match index {
+            Some(index) => index,
+            None => testi32.len(),
+        };
+        while testi32.len() <= index {
+            testi32.resize_with(100, || RwLock::new(0));
+        }
+        return index;
     }
+
+    let mut get_value = |index: usize| {
+        return testi32[index].read().unwrap().clone();
+    };
+
+    register_value(None, 1);
+
+    println!("Hello, world! {:?}", testi32)
+    // let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+
+    // for stream in server.incoming() {
+    //     spawn(move || {
+    //         let mut websocket = accept(stream.unwrap()).unwrap();
+    //         loop {
+    //             let msg = websocket.read().unwrap();
+
+    //             // We do not want to send back ping/pong messages.
+    //             if msg.is_binary() || msg.is_text() {
+    //                 print!("Received: {}", msg);
+    //                 websocket.send(msg).unwrap();
+    //             }
+    //         }
+    //     });
+    // }
 }
